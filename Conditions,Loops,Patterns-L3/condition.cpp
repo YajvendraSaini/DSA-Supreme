bceeb4646872a0bb70b3df65c6a87031e3a28462
Yajvendra <yajvendra98@gmail.com>
2023-12-09 06:49:52 +0530

    nested if-else statement

--- Conditions,Loops,Patterns-L3/condition.cpp
+++ Conditions,Loops,Patterns-L3/condition.cpp
@@ -19,18 +19,52 @@ using namespace std;
 
 //if-else statement
 
+// int main(){
+//     int age;
+
+//     cout<<"Enter your age"<<endl;
+//     cin>>age;
+
+//     if(age>18){
+//         cout<<"you can vote."<<endl;
+//     }
+//     else{
+//         cout<<"You can not vote."<<endl;
+//     }
+// }
+
+//Nested if-else statement
+
 int main(){
-    int age;
+    int marks;
 
-    cout<<"Enter your age"<<endl;
-    cin>>age;
+    cout<<"Enter your marks"<<endl;
+    cin >> marks;
 
-    if(age>18){
-        cout<<"you can vote."<<endl;
+    if(marks>=90){
+        cout<<"You got A"<<endl;
     }
     else{
-        cout<<"You can not vote."<<endl;
-    }
-}
+        if(marks>=80){
+            cout<<"You got B"<<endl;
+        }
+        else{
+            if(marks>=50){
+                cout<<"You got C"<<endl;
+            }
+            else{
+                if(marks>=30){
+                    cout<<"You got D"<<endl;
+                }
+                else{
+                    if(marks<30){
+                        cout<<"You got F"<<endl;
+                    }
+                }
 
+            }
+        }
+    }
+
+}
 
